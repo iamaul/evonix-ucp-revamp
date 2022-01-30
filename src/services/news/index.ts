7d3c26f6f6681413b5f1e3effb7e5ebde4e6ac96
Agung Maulana <iamaul@hotmail.com>
2022-01-30 14:19:26 +0700

    fix: news-response imports

--- src/services/news/index.ts
+++ src/services/news/index.ts
@@ -1,10 +1,8 @@
-import { NewsHeadlineResponse } from "@/types/news-response";
+import { NewsResponse } from "@/types/news-response";
 import { apiGet, useApiSWR } from "@/utils/swr-api";
 
 const NEWS_PATH = "/api/v1/news";
 const HEADLINE_NEWS_PATH = "/api/v1/news/headline";
-
-export type NewsResponse = NewsHeadlineResponse;
 
 export const getNewsList = () => apiGet<NewsResponse>(NEWS_PATH);
 export const getNewsHeadlineList = () =>
