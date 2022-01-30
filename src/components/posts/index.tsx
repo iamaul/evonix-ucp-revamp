49437a06f91e02fcac9bd4b3a987f509cd0a2555
Agung Maulana <iamaul@hotmail.com>
2022-01-30 22:27:28 +0700

    add: Tooltip over post cards

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -5,6 +5,7 @@ import {
   Text,
   useColorModeValue,
   VStack,
+  Tooltip,
 } from "@chakra-ui/react";
 import parse from "html-react-parser";
 import Link from "next/link";
@@ -21,46 +22,48 @@ const PostList = ({ news }: PostListProps) => {
     <>
       {news?.map((data) => (
         <Link href={`/post/${data.slug}`} passHref>
-          <Stack
-            w="100%"
-            rounded="lg"
-            borderWidth="1px"
-            bg={bgColor}
-            borderColor={borderColor}
-            p={6}
-            spacing={4}
-            cursor="pointer"
-            transition="all 0.25s"
-            transition-timing-function="spring(1 100 10 10)"
-            _hover={{ transform: "translateY(-4px)", shadow: "md" }}
-          >
-            <HStack
-              spacing={{ base: 3, md: 8 }}
+          <Tooltip label="Read more">
+            <Stack
               w="100%"
-              justifyContent="start"
+              rounded="lg"
+              borderWidth="1px"
+              bg={bgColor}
+              borderColor={borderColor}
+              p={6}
+              spacing={4}
+              cursor="pointer"
+              transition="all 0.25s"
+              transition-timing-function="spring(1 100 10 10)"
+              _hover={{ transform: "translateY(-4px)", shadow: "md" }}
             >
-              <Image
-                src={data.image}
-                borderRadius="sm"
-                boxSize={{ base: "80px", md: "150px" }}
-                boxShadow="xs"
-              />
-              <VStack align="start">
-                <Text fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>
-                  {data.title}
-                </Text>
-                <Text
-                  fontSize={["md", null, "sm"]}
-                  align="justify"
-                  color={textColor}
-                >
-                  <Truncate lines={2} ellipsis={<span>...</span>}>
-                    {parse(data.content)}
-                  </Truncate>
-                </Text>
-              </VStack>
-            </HStack>
-          </Stack>
+              <HStack
+                spacing={{ base: 3, md: 8 }}
+                w="100%"
+                justifyContent="start"
+              >
+                <Image
+                  src={data.image}
+                  borderRadius="sm"
+                  boxSize={{ base: "80px", md: "150px" }}
+                  boxShadow="xs"
+                />
+                <VStack align="start">
+                  <Text fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>
+                    {data.title}
+                  </Text>
+                  <Text
+                    fontSize={["md", null, "sm"]}
+                    align="justify"
+                    color={textColor}
+                  >
+                    <Truncate lines={2} ellipsis={<span>...</span>}>
+                      {parse(data.content)}
+                    </Truncate>
+                  </Text>
+                </VStack>
+              </HStack>
+            </Stack>
+          </Tooltip>
         </Link>
       ))}
     </>
